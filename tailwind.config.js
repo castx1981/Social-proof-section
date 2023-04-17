b1e8373196fbb422861540d334fb1588f122e5e9
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-17 14:25:05 +0800

    add background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,8 @@ module.exports = {
         'very-dark-magenta': 'hsl(300, 43%, 22%)',
         'soft-pink': 'hsl(333, 80%, 67%)',
         // neutral
-        'Dark Grayish Magenta': 'hsl(303, 10%, 53%)',
+        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
         'light-grayish-magenta': 'hsl(300, 24%, 96%)',
-      },
-      width: {
-        'mobile': '375px',
       },
     }
   },
