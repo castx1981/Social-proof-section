bcc245d09a842b4c115404c4ffa52dc3df7a04b0
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-17 12:15:30 +0800

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['League Spartan', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        // primary
+        'very-dark-magenta': 'hsl(300, 43%, 22%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+        // neutral
+        'Dark Grayish Magenta': 'hsl(303, 10%, 53%)',
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+      },
+      width: {
+        'mobile': '375px',
+      },
+    }
+  },
+  plugins: [],
+}
