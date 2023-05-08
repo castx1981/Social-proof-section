9efb09174f10f6fb22d0b132a1c71e431d796a04
Xiaoliang Luan <xluan@qjnext.com>
2023-05-09 00:36:41 +0800

    Restyle based on figma design

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,20 @@ module.exports = {
         'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
         'light-grayish-magenta': 'hsl(300, 24%, 96%)',
       },
+      fontSize: {
+        'lg': ['19px', {
+          lineHeight: '25px',
+          letterSpacing: '-0.63px',
+        }],
+        '4xl': ['40px', {
+          lineHeight: '32px',
+          letterSpacing: '-1.43px',
+        }],
+        '6xl': ['56px', {
+          lineHeight: '48px',
+          letterSpacing: '-2px',
+        }],
+      }
     }
   },
   plugins: [],
